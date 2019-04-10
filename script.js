f6a81023d06830046ecab448c205d2bc86efb0b4
Willem van der Merwe <iqabsent@gmail.com>
2019-04-10 14:58:02 +0100

    Add card data; wireframe form sumbmit handler

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-function onFormSubmit(e) {
+const cards = [
+    {
+        name: "Student Life",
+        apr: 18.9,
+        btDuration: 0,
+        purchaseDuration: 6,
+        credit: 1200,
+        requirements: {
+            occupationIs: "student"
+        }
+    },
+    {
+        name: "Anywhere Card",
+        apr: 33.9,
+        bt_duration: 0,
+        p_duration: 0,
+        credit: 300,
+        requirements: {}
+    },
+    {
+        name: "Liquid Card",
+        apr: 33.9,
+        bt_duration: 12,
+        p_duration: 6,
+        credit: 3000,
+        requirements: {
+            minIncome: 16000
+        }
+    }
+]
+
+function requirementsMet(card, applicantData) {
+    return true; // TODO, actual filtering
+}
+
+function handleFormSubmit(e) {
     e.preventDefault()
-    console.log('yes, hi')
+    const formData = {
+        // just what we care about .. for now
+        income: document.querySelector('[name=income]').value,
+        occupation: document.querySelector('[name=occupation]:checked').value
+    }
+
+    console.log('FORM DATA: ', formData)
 }
